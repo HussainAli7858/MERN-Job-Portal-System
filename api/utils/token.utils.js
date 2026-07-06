e728e8d0c4cf6d074c8d0a4e2f75e0b809a02815
HussainAli7858 <hussain7858alisabir@gmail.com>
2026-07-06 11:13:49 +0500

    add access token and refresh token logic

---
+++ api/utils/token.utils.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken";
+
+export const generateAccessToken = (accountId) => {
+  return jwt.sign(
+    { id: accountId },
+    process.env.JWT_ACCESS_SECRET,
+    { expiresIn: process.env.JWT_ACCESS_EXPIRES_IN }
+  );
+};
+
+export const generateRefreshToken = (accountId) => {
+  return jwt.sign(
+    { id: accountId },
+    process.env.JWT_REFRESH_SECRET,
+    { expiresIn: process.env.JWT_REFRESH_EXPIRES_IN }
+  );
+};
+
+export const verifyAccessToken = (token) => {
+  return jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+};
+
+export const verifyRefreshToken = (token) => {
+  return jwt.verify(token, process.env.JWT_REFRESH_SECRET);
+};
